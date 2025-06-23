4b5c64a1abcacd19b789691b862169b8198d26f6
ali zeinali <ali.zeinali70@gmail.com>
2025-06-23 21:29:52 +0200

    add admin components

--- Aliz.Client/src/app/app.routes.ts
+++ Aliz.Client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AdminLayoutComponent } from './components/admin/admin-layout/admin-layout.component';
 
 export const routes: Routes = [
-    {path:'',pathMatch:'full',component:HomeComponent},
-    {path:'login',component:LoginComponent}
+    { path: '', pathMatch: 'full', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'admin', component: AdminLayoutComponent }
 ];
